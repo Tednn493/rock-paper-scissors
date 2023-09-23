3101b87525b078218e06fd45e235aa8b127fbf61
Tednn493 <Tednn493@gmail.com>
2023-09-23 13:04:24 +0100

    Created basic UI

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const buttons= document.querySelectorAll('button');
+const scoreboard = document.querySelector('.scoreboard');
+var playerSelection = " ";
+var computerSelection = " ";
+var playerScore = 0
+var computerScore = 0 
+
+function updateScoreboard(){
+    if (playerScore===5){
+        scoreboard.textContent = "You win!";
+    }else if (computerScore===5){
+        scoreboard.textContent = "Computer wins!"
+    }else{
+        scoreboard.textContent = `PLAYER:${playerScore} AI:${computerScore}`
+    }
+}
+
+function playerChoice(e){
+  playerSelection = this.classList.value;
+  computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection);
+} ;
+
+
 function getComputerChoice() {
     const choice= Math.floor(Math.random()*3)
     
@@ -7,31 +31,24 @@ function getComputerChoice() {
         return "paper"
     }else {
         return "scissors"
-    }
-}
-
+    };
+};
 
 
 function playRound(pChoice, cChoice) {
     if (cChoice===pChoice){
-        return "It's a tie!"
-    } else if (cChoice==="rock" && pChoice==="paper"){
-        return "You won!"
-    }else if (cChoice==="rock" && pChoice==="scissors"){
-        return "You lost!"
-    }else if (cChoice==="paper" && pChoice==="scissors"){
-        return "You won!"
-    }else if (cChoice==="paper" && pChoice==="rock"){
-        return "You lost!"
-    }else if (cChoice==="scissors" && pChoice==="rock"){
-        return "You won!"
-    }else if (cChoice==="scissors" && pChoice==="paper"){
-        return "You lost!"
-    }
-  }
+        console.log("It's a tie!");
+    } else if ((cChoice==="rock" && pChoice==="paper") || (cChoice==="paper" && pChoice==="scissors") || (cChoice==="scissors" && pChoice==="rock")){
+        console.log("You won!");
+        playerScore++;
+        updateScoreboard();
+    }else{
+        console.log("You lost!");
+        computerScore++;
+        updateScoreboard();
+    };
+  };
 
-  const computerSelection = getComputerChoice();
-  const playerSelection = prompt().toLowerCase();
+
   
-  console.log(computerSelection)
-  console.log(playRound(playerSelection, computerSelection));
+  buttons.forEach(button => button.addEventListener('click',playerChoice));
